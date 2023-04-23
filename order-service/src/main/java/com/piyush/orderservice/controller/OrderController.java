a6f388340311e2f2580ba424cb575bc75674792e
pishivam009 <pishivam009@gmail.com>
2023-04-24 00:45:22 +0530

    Added Log Lifes

--- order-service/src/main/java/com/piyush/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/piyush/orderservice/controller/OrderController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.piyush.orderservice.dto.OrderRequest;
 import com.piyush.orderservice.service.OrderService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
@@ -20,9 +23,18 @@ public class OrderController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
+	@CircuitBreaker(name="inventory" , fallbackMethod = "fallbackMethod")
+	//Circuit breaker pattern applied here
+	@TimeLimiter(name = "inventory")
+	//name should be same as the one used in application.properties
 	public String placeOrder(@RequestBody OrderRequest orderRequest) {
 		orderService.placeOrder(orderRequest);
 		return "Order Placed Successfully";
+	}
+	
+	//fallbackMethod should have same return type as method
+	public String fallbackMethod(@RequestBody OrderRequest orderRequest, RuntimeException runtimeException) {
+		return "Oops! Something went wrong. Please order after some time";
 	}
 	
 }
